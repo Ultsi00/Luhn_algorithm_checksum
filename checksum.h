648eee70a3dede4eda838f15aff228bf13c16153
Timo T <60548494+Ultsi00@users.noreply.github.com>
2024-01-03 03:30:05 +0200

    Add files via upload

--- checksum.h
+++ checksum.h
@@ -8,43 +8,30 @@ using namespace std;
 class Checker {
     public:
         const char* mStr;
-        uint8_t mLen;
         short int *seq;
+        uint8_t mLen;
         uint16_t mSum;
         uint8_t mChecksum;
-
         Checker() {}
-        //len and checksum print null
-        // const char* was needed for \0
-        // string is object which can use .length()
-        // -> rethink if use only string
-        // -> maybe create Checker object in program while loop after Selection object,
-        //      using mInput as an argument in Checker(arg)
+        ~Checker() { cout << "Destructor utilized" << endl; }
+
         inline void setString(const char* str) { mStr = str; }
 
         void processChecksum() {
-            cout << "initial mStr: " << mStr << endl;
             countLen();
-            cout << "len:" << mLen << endl;
             inputToIntArr();
-            cout << "seq 1 2 3:" << seq[0] << seq[1] << seq[2] << endl;
             indexToMultiply();
-            cout << "seq 1 2 3 after multiply:" << seq[0] << seq[1] << seq[2] << endl;
             toSum();
-            cout << "mSum:" << mSum << endl;
             countAndPrintChecksum();
-            //free *seq
+            delete seq;
         }
-
         inline void countLen() {
             mLen = 0;
-            cout << "mStr0: " << mStr[0] << endl; //
-            cout << "len:" << mLen << endl; //
             for (int i = 0; mStr[i] != '\0'; i++)
-                mLen++;
+                mLen++;  
         }
         inline void inputToIntArr() {
-            seq = (short int *)malloc(sizeof(short int) * (mLen - 1));
+            seq = new short int[mLen];
             for (int i = 0; *mStr != '\0'; i++) { 
                 seq[i] = *mStr - '0';
                 mStr++;
@@ -69,7 +56,9 @@ class Checker {
         }
         inline void countAndPrintChecksum() {
             mChecksum = 10 - (mSum % 10);
-            cout << "Checksum is: " << mChecksum << endl;
+            //Unary '+' operator (in uint8_t mChecksum) promotes the primitive datatype to int,
+            // resulting in a printable numerical value, not ASCII
+            cout << "Checksum is: " << +mChecksum << endl;
         }
 };
 
@@ -80,18 +69,17 @@ class Selection {
 
         Selection() : mInput("0") {}
 
-        inline void instructions() {
-            cout << "Choose an option: 1 or 2" << endl;
+        inline void printInstructions() {
+            cout << "\nChoose an option: 1 or 2 ['q' to quit]" << endl;
             cout << "(1.) Enter a number sequence\n\tOR\n" 
-                    << "(2.) Premade sequences:" << endl;
-            cout << "\t(a) 123456789\n\t(b) 59210\n\t(c) 7649278" << endl;
-            cout << "['q' to quit]" << endl;
+                    << "(2.) Choose a premade sequence" << endl;
         }
 
-        inline void menuStart() {
-            char mSelection = 0;
+        inline short int menuStart() {
+            char mSelection = '0';
             while(mSelection != 'q') {
-                instructions();
+                printInstructions();
+                cout << "Selection: ";
                 cin >> mSelection;
                 if (mSelection != '1' && mSelection != '2' && mSelection != 'q')
                     cout << "\n\t*** False input ***\n" << endl;
@@ -102,24 +90,26 @@ class Selection {
                     case('2'):
                         menuPremade();
                         break;
+                    case('q'):
+                        return 0;
                     default:
                         ;
                 }
                 if (mSelection == '1' || mSelection == '2')
                     break;        
             }
+            return 1;
         }
 
         inline void menuPremade()  {
-            char sel_premade = 0;
-            while (sel_premade != 'a' && sel_premade != 'b' 
-                    && sel_premade != 'c' && sel_premade != 'q')
-            {
-                cout << "Select a premade sequence: [a/b/c]" << endl;
+            char sel_premade = '0';
+            while (sel_premade != 'a' && sel_premade != 'b' && sel_premade != 'c') {
+                cout << "\nChoose a premade sequence: [a/b/c]" << endl;
                 cout << "\t(a) 123456789\n\t(b) 59210\n\t(c) 7649278" << endl;
+                cout << "Selection: ";
                 cin >> sel_premade;
                 if (sel_premade != 'a' && sel_premade != 'b' && sel_premade != 'c')
-                    cout << "\n\t*** False input ***\n" << endl;
+                    cout << "\n***** Error: False input *****\n" << endl;
                 switch(sel_premade) {
                     case('a'):
                         mInput = "123456789";
@@ -137,57 +127,23 @@ class Selection {
         }
 
         inline void menuFree() {
-            while (mInputTemp.length() < 1) {
+            bool error;
+            while (true) {
+                error = false;
+                cout << "\nInsert number sequence: ";
                 cin >> mInputTemp;
-                mInput = mInputTemp.c_str();
-                cout << mInput << endl;
+                for (int i = 0; i < mInputTemp.size(); i++) {
+                    if (isdigit(mInputTemp[i]) != true)
+                        error = true;
+                }
+                if (error == true)
+                    cout << "\n***** Error: False input *****\n" << endl; 
+                else {
+                    mInput = mInputTemp.c_str();
+                    break;
+                }
             }
         }
 };
 
 #endif
-
-
-
-
-
-/* Algo C style
-        //sequence
-    const char* b = "123456789";
-    
-        //len
-    int len = 0;
-    for (int i = 0; b[i] != '\0'; i++) {
-        len++;
-    }
-
-        //char[] to int[]
-    int seq[len - 1];
-    for (int i = 0; *b != '\0'; i++) {
-        seq[i] = *b - '0';
-        b++;
-    }
-
-    // multiply
-    int toMultiply = (len - 2);
-    while(toMultiply >= 0) {
-        seq[toMultiply] *= 2;
-        toMultiply -= 2;
-    }
-
-    //sum
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        if (seq[i] >= 10) {
-            sum = sum + (1 + (seq[i] % 10));
-        }
-        else
-            sum = sum + seq[i];
-    }
-
-    //checksum
-    int checksum = 0;
-    checksum = 10 - (sum % 10);
-    cout << checksum << endl;
-    
-*/
--- main.cpp
+++ main.cpp
@@ -1,27 +1,43 @@
 #include "checksum.h"
 
 /*
-    reverse order
-    multiply every second from right side
-    sum digits, e.g. 14 = 1 + 4 = 5 / 4 = 4
-    sum all
-    checksum: 10  - (sum mod 10)
+    User can:
+        1) insert a number sequence
+        2) select a premade number sequence
+    By replicating Luhn algorithm, the Checksum of the sequence is calculated.
+
+    Algorithm:
+    - from the right hand side of the number sequence, every second
+        number is multiplied by 2. For example:
+        12345   ==>>    1 2*2 3 4*2 5
+        6428    ==>>    6*2 4 2*2 8
+    - if the product is 2-digit (>=10 and <=18), the two digits are summed. 
+        For example: 7113 ==>> 7*2 1 1*2 3 ==>> 14 1 2 3 ==> 1+4 1 2 3==> 5 1 2 3 
+    - now all the numbers are summed (5 1 2 3 ==>> 11 (=sum))
+    - checksum (=9): 
+        10 - (sum mod 10) ==>> 10 - (11 mod 10) ==>> 10 - 1 ==>> __9__
 */
 
-void program_loop() {
-    Checker checker = Checker();
+void luhnAlgorithm() {
     Selection selection = Selection();
-   
-    //while()
-    selection.menuStart();
-    checker.setString(selection.mInput);
-    checker.processChecksum();
+    Checker checker = Checker();
+    char reset;
 
+    while(true) {
+        reset = '0';
+        if (selection.menuStart() == 0)
+            break;
+        checker.setString(selection.mInput);
+        checker.processChecksum();
+        cout << "\nReset? [y/n]: ";
+        while (reset != 'y' && reset != 'n')
+            cin >> reset;
+        if (reset == 'n')
+            break;
+    }
 }
 
 int main() {
-
-    program_loop();
-
+    luhnAlgorithm();
     return 0;
 }
